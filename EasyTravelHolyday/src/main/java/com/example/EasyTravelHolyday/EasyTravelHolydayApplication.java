2597b8ecd35fb10b270d14a262497239c6b7ac47
SamiMas24 <samirahmasimina@gmail.com>
2022-10-16 13:52:23 +0200

    add project

---
+++ EasyTravelHolyday/src/main/java/com/example/EasyTravelHolyday/EasyTravelHolydayApplication.java
@@ -0,0 +1,13 @@
+package com.example.EasyTravelHolyday;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyTravelHolydayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasyTravelHolydayApplication.class, args);
+	}
+
+}
